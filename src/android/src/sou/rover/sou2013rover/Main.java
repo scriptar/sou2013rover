a5808d1eee513401a237def01a68006b6b650f0e
Ryan Dempsey <Ryan5732@gmail.com>
2013-02-20 20:54:37 -0800

    Added initial bluetooth testing

--- src/android/src/sou/rover/sou2013rover/Main.java
+++ src/android/src/sou/rover/sou2013rover/Main.java
@@ -1,22 +1,70 @@
 package sou.rover.sou2013rover;
 
+import java.util.Set;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 //Initial Activity -- Will be main screen, with menu options and instructions on connecting rover.
 public class Main extends Activity {
+	
+    private static final int REQUEST_ENABLE_BT = 5;
 
-    @Override
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
     	//called when activity is first created
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.Button01);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Log.v("test", "Button Pressed");
+            	testBluetooth();
+            }
+        });
+        
     }
 
+    protected void testBluetooth(){
+    	Log.v("test", "Bluetooth test");
+    	
+    	BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    	if (mBluetoothAdapter == null) {
+    	    // Device does not support Bluetooth
+        	Log.v("test", "Device does not support Bluetooth");
+        	return;
+    	}
+    	
+    	if (!mBluetoothAdapter.isEnabled()) {
+        	Log.v("test", "Bluetooth Not Enabled, Enabling");
+    	    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+    	    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    	} else {
+        	Log.v("test", "Bluetooth already Enabled");
+    	}
+    	
+    	Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+    	// If there are paired devices
+    	if (pairedDevices.size() > 0) {
+    	    // Loop through paired devices
+    	    for (BluetoothDevice device : pairedDevices) {
+				// Add the name and address to an array adapter to show in a ListView
+    	        // mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+    	    }
+    	}
+    	
+    }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
