5ccb3157dfe1d998162650cef30b840582a79830
Ryan5732 <Ryan5732@gmail.com>
2013-02-22 20:47:26 -0800

    Added Logo & Bluetooth Connection
    The program now connects to a single Arduino Bluetooth Object. The
    chip's red light stops blinking and instead goes green. I've got
    input/output streams... just need to figure out how to use them.

--- src/android/src/edu/sou/rover2013/ScreenBluetooth.java
+++ src/android/src/edu/sou/rover2013/ScreenBluetooth.java
@@ -1,12 +1,14 @@
 package edu.sou.rover2013;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
-
-import edu.sou.rover2013.R;
+import java.util.UUID;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,20 +16,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class ScreenBluetooth extends Activity {
 
-
 	private static final int REQUEST_ENABLE_BT = 5;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_screen_bluetooth);
-		
+
 		// in-line button listener
 		final Button button = (Button) findViewById(R.id.button_test_bluetooth);
 		button.setOnClickListener(new View.OnClickListener() {
@@ -40,12 +39,12 @@ public class ScreenBluetooth extends Activity {
 	}
 
 	@Override
-	// Inflate the menu; adds items to the action bar if it is present.	
+	// Inflate the menu; adds items to the action bar if it is present.
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.settings_menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	// Handles menu item selection, returns true if successful
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -74,11 +73,11 @@ public class ScreenBluetooth extends Activity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
 	// Just a temporary function where I'm trying to hash out how Bluetooth
-	// functions in Android. Will be moved. --Ryan
+	// functions in Android. Will be broken up and moved. --Ryan
 	protected void testBluetooth() {
-		
+
 		// Check to see if bluetooth exists
 		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
 				.getDefaultAdapter();
@@ -101,21 +100,42 @@ public class ScreenBluetooth extends Activity {
 		// Sort/Select available bluetooth paired devices.
 		Set<BluetoothDevice> pairedDevices = mBluetoothAdapter
 				.getBondedDevices();
-		
+
 		// If there are paired devices
-		Log.d("test", "Device Count: "+pairedDevices.size());
+		Log.d("test", "Device Count: " + pairedDevices.size());
+		BluetoothDevice lastDevice = null;
 		if (pairedDevices.size() > 0) {
 			// Loop through paired devices
-			TextView text = (TextView)findViewById(R.id.textView2);
+			TextView text = (TextView) findViewById(R.id.textView2);
 			text.setText("");
 			text.append("List of paired devices:\n");
+
 			for (BluetoothDevice device : pairedDevices) {
-			//For each device, add name & address to label
-				text.append(device.getName()+" "+device.getAddress()+"\n");
+				// For each device, add name & address to label
+				text.append(device.getName() + " " + device.getAddress() + "\n");
+				lastDevice = device;
 			}
 
-		}
+			UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard
+																					// SerialPortService
+																					// ID
+			BluetoothSocket mmSocket;
+			OutputStream mmOutputStream;
+			InputStream mmInputStream = null;
+			try {
+				mmSocket = lastDevice.createRfcommSocketToServiceRecord(uuid);
+				mmSocket.connect();
+				mmOutputStream = mmSocket.getOutputStream();
+				mmInputStream = mmSocket.getInputStream();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Log.v("test", "Bluetooth connected.");
+			
+			//do {
+			//	text.append(mmInputStream.toString());
+			//} while (true);
 
+		}
 	}
-	
 }
