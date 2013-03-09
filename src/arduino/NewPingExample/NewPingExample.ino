1bef9775d3c7ef98a1656a736fb584f49c82d003
Shaun Wolff <wolffs2@sou.edu>
2013-03-09 00:47:50 -0800

    logoish movement functions

--- src/arduino/NewPingExample/NewPingExample.ino
+++ src/arduino/NewPingExample/NewPingExample.ino
@@ -11,38 +11,19 @@
 #define MAX_DISTANCE 10 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-//Servo myservo;
-long pos = 0;
-int i = 0;
-int n = 0;
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-//  myservo.attach(9);
 }
 
 void loop() {
   delay(200);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   unsigned int uS = sonar.ping();  // Send ping, get ping time in microseconds (uS).
   int pinVal = digitalRead(2);
-//  Serial.print("Ping: ");
-//  Serial.print(uS / US_ROUNDTRIP_CM); // Convert ping time to distance in cm and print result (0 = outside set distance range)
-//  Serial.println("cm");
+  Serial.print("Ping: ");
+  Serial.print(uS / US_ROUNDTRIP_CM); // Convert ping time to distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
   Serial.print("Pin 2: ");
   Serial.println(pinVal);
-//  pos = uS / US_ROUNDTRIP_CM;
-//  if(pos < 50 && pos > 0) {
-////    delay(1000);
-//    for(n = 0; n < 2; n += 1) {
-//    for(i = 0; i < 160; i += 1) {
-//      myservo.write(i);
-//      delay(2);
-//    }
-//    for(i = 160; i > 20; i -= 1) {
-//      myservo.write(i);
-//      delay(2);
-//    }
-//    }    
-//  }
-  
+
 }
