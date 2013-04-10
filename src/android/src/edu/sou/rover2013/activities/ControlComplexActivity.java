df36fbc032dfa650d4d0edd6c5fb1c631b631b4c
Ryan Dempsey <Ryan5732@gmail.com>
2013-04-09 20:53:00 -0700

    Completely revamped the complex control interface
    Much better... Also removed outdated arduino code.

--- src/android/src/edu/sou/rover2013/activities/ControlComplexActivity.java
+++ src/android/src/edu/sou/rover2013/activities/ControlComplexActivity.java
@@ -3,11 +3,17 @@ package edu.sou.rover2013.activities;
 import edu.sou.rover2013.BaseActivity;
 import edu.sou.rover2013.R;
 import edu.sou.rover2013.models.Rover;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * 
@@ -19,23 +25,34 @@ import android.widget.EditText;
  */
 public class ControlComplexActivity extends BaseActivity {
 
+	// Storing this activities context for use in dialogs
+	Context activityContext;
+
 	// UI Elements
-	private EditText scriptTextBox;
-	private Button forward;
-	private Button reverse;
-	private Button left;
-	private Button right;
-	private Button clear;
-	private Button sample1;
-	private Button sample2;
-	private Button sample3;
-	private Button transmit;
+	private static EditText scriptTextBox;
+	private static Button button_laser;
+	private static Button button_logic;
+	private static Button button_variables;
+	private static Button button_program_flow;
+	private static Button button_pause;
+	private static Button button_tests;
+	private static Button button_variable_math;
+	private static Button button_arithmetic;
+	private static Button button_forward;
+	private static Button button_reverse;
+	private static Button button_left;
+	private static Button button_right;
+	private static Button button_save;
+	private static Button button_load;
+	private static Button button_clear;
+	private static Button button_send;
 
 	// Rover Model
 	private Rover rover;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// Expanding XML
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.control_complex);
 
@@ -43,68 +60,360 @@ public class ControlComplexActivity extends BaseActivity {
 		// Assigning UI Elements
 		// *******************************
 		scriptTextBox = (EditText) findViewById(R.id.complex_script);
-		forward = (Button) findViewById(R.id.complex_forward);
-		reverse = (Button) findViewById(R.id.complex_reverse);
-		left = (Button) findViewById(R.id.complex_left);
-		right = (Button) findViewById(R.id.complex_right);
-		clear = (Button) findViewById(R.id.complex_clear);
-		sample1 = (Button) findViewById(R.id.complex_sample_one);
-		sample2 = (Button) findViewById(R.id.complex_sample_two);
-		sample3 = (Button) findViewById(R.id.complex_sample_three);
-		transmit = (Button) findViewById(R.id.complex_transmit);
-		
+		button_laser = (Button) findViewById(R.id.complex_laser);
+		button_logic = (Button) findViewById(R.id.complex_logic);
+		button_variables = (Button) findViewById(R.id.complex_variables);
+		button_program_flow = (Button) findViewById(R.id.complex_flow);
+		button_pause = (Button) findViewById(R.id.complex_pause);
+		button_tests = (Button) findViewById(R.id.complex_tests);
+		button_variable_math = (Button) findViewById(R.id.complex_variable_math);
+		button_arithmetic = (Button) findViewById(R.id.complex_arithmetic);
+		button_forward = (Button) findViewById(R.id.complex_forward);
+		button_reverse = (Button) findViewById(R.id.complex_reverse);
+		button_left = (Button) findViewById(R.id.complex_left);
+		button_right = (Button) findViewById(R.id.complex_right);
+		button_save = (Button) findViewById(R.id.complex_save);
+		button_load = (Button) findViewById(R.id.complex_load);
+		button_clear = (Button) findViewById(R.id.complex_clear);
+		button_send = (Button) findViewById(R.id.complex_transmit);
+
 		// *******************************
 		// Button Listeners
 		// *******************************
-		forward.setOnClickListener(new View.OnClickListener() {
+		button_laser.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				scriptAppend("fd 2\n");
+
 			}
 		});
-		reverse.setOnClickListener(new View.OnClickListener() {
+		button_logic.setOnClickListener(new View.OnClickListener() {
+			// Creates a selector Dialog Box
+			final String selectTrue = "True";
+			final String selectFalse = "False";
+			final String selectAnd = "And";
+			final String selectOr = "Or";
+			final CharSequence[] list = { selectTrue, selectFalse, selectAnd,
+					selectOr };
+
 			public void onClick(View v) {
-				scriptAppend("bk 2\n");
+				AlertDialog.Builder chooserBox = new AlertDialog.Builder(
+						activityContext);
+				chooserBox.setTitle("Select a logical operator:");
+				chooserBox.setItems(list,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								if (list[item].equals(selectTrue)) {
+									scriptInsertAtCursor("True ");
+								} else if (list[item].equals(selectFalse)) {
+									scriptInsertAtCursor("False ");
+								} else if (list[item].equals(selectAnd)) {
+									scriptInsertAtCursor("And ");
+								} else if (list[item].equals(selectOr)) {
+									scriptInsertAtCursor("Or ");
+								}
+							}
+						});
+				AlertDialog alert = chooserBox.create();
+				alert.show();
 			}
 		});
-		left.setOnClickListener(new View.OnClickListener() {
+		button_variables.setOnClickListener(new View.OnClickListener() {
+			// Creates a selector Dialog Box
+			final CharSequence[] list = { "V0", "V1", "V2", "V3", "V4", "V5",
+					"V6", "V7", "V8", "V9", "V10" };
+
 			public void onClick(View v) {
-				scriptAppend("lt 2\n");
+				AlertDialog.Builder chooserBox = new AlertDialog.Builder(
+						activityContext);
+				chooserBox.setTitle("Select a variable:");
+				chooserBox.setItems(list,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								scriptInsertAtCursor(list[item].toString()
+										+ " ");
+							}
+						});
+				AlertDialog alert = chooserBox.create();
+				alert.show();
 			}
 		});
-		right.setOnClickListener(new View.OnClickListener() {
+		button_program_flow.setOnClickListener(new View.OnClickListener() {
+			// Creates a selector Dialog Box
+			private final String selectIf = "If (Test) [Code]";
+			private final String selectElse = "If Else (Test) [Code]";
+			private final String selectRepeat = "Repeat (Value) [Code]";
+			final CharSequence[] list = { selectIf, selectElse, selectRepeat };
+
 			public void onClick(View v) {
-				scriptAppend("rt 2\n");
+				AlertDialog.Builder chooserBox = new AlertDialog.Builder(
+						activityContext);
+				chooserBox.setTitle("Select a program flow structure:");
+				chooserBox.setItems(list,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								if (list[item].equals(selectIf)) {
+									scriptInsertAtCursor("If ( ) [ ] ");
+								} else if (list[item].equals(selectElse)) {
+									scriptInsertAtCursor("If Else ( ) [ ] ");
+								} else if (list[item].equals(selectRepeat)) {
+									scriptInsertAtCursor("Repeat value [ ] ");
+								}
+							}
+						});
+				AlertDialog alert = chooserBox.create();
+				alert.show();
 			}
 		});
-		clear.setOnClickListener(new View.OnClickListener() {
+		button_pause.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						activityContext);
+				alert.setTitle("Pause/Wait");
+				alert.setMessage("How long should the rover pause, in milliseconds?");
+				// Set an EditText view to get user input
+				final EditText input = new EditText(activityContext);
+				input.setInputType(InputType.TYPE_CLASS_NUMBER);
+				alert.setView(input);
+				alert.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								String value = input.getText().toString();
+								scriptInsertAtCursor("Pause " + value + " ");
+							}
+						});
+
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// Nothing Happens if closed
+							}
+						});
+
+				alert.show();
+			}
+		});
+		button_tests.setOnClickListener(new View.OnClickListener() {
+			// Creates a selector Dialog Box
+			private final String selectEquals = "=";
+			private final String selectNotEquals = "<>";
+			private final String selectGreater = ">";
+			private final String selectLess = "<";
+			private final String selectGreaterEqual = ">=";
+			private final String selectLessEqual = "<=";
+			final CharSequence[] list = { selectEquals, selectNotEquals,
+					selectGreater, selectLess, selectGreaterEqual,
+					selectLessEqual };
+
+			public void onClick(View v) {
+				AlertDialog.Builder chooserBox = new AlertDialog.Builder(
+						activityContext);
+				chooserBox.setTitle("Select a comparison test operator:");
+				chooserBox.setItems(list,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								if (list[item].equals(selectEquals)) {
+									scriptInsertAtCursor("= ");
+								} else if (list[item].equals(selectNotEquals)) {
+									scriptInsertAtCursor("<> ");
+								} else if (list[item].equals(selectGreater)) {
+									scriptInsertAtCursor("> ");
+								} else if (list[item].equals(selectLess)) {
+									scriptInsertAtCursor("< ");
+								} else if (list[item]
+										.equals(selectGreaterEqual)) {
+									scriptInsertAtCursor(">= ");
+								} else if (list[item].equals(selectLessEqual)) {
+									scriptInsertAtCursor("<= ");
+								}
+							}
+						});
+				AlertDialog alert = chooserBox.create();
+				alert.show();
+			}
+		});
+		button_variable_math.setOnClickListener(new View.OnClickListener() {
+			// Creates a selector Dialog Box
+			private final String selectAdd = "Add a value to a Variable";
+			private final String selectSub = "Subtract a value from a Variable";
+			private final String selectMul = "Multiply a value with a Variable";
+			private final String selectDiv = "Divide a Variable by a value";
+			final CharSequence[] list = { selectAdd, selectSub, selectMul,
+					selectDiv };
+			public void onClick(View v) {
+				AlertDialog.Builder chooserBox = new AlertDialog.Builder(
+						activityContext);
+				chooserBox
+						.setTitle("Select a math operator to apply to a variable:");
+				chooserBox.setItems(list,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								if (list[item].equals(selectAdd)) {
+									scriptInsertAtCursor("Add V0 0 ");
+								} else if (list[item].equals(selectSub)) {
+									scriptInsertAtCursor("Sub V0 0 ");
+								} else if (list[item].equals(selectMul)) {
+									scriptInsertAtCursor("Mul V0 0 ");
+								} else if (list[item].equals(selectDiv)) {
+									scriptInsertAtCursor("Div V0 0 ");
+								}
+							}
+						});
+				AlertDialog alert = chooserBox.create();
+				alert.show();
+			}
+		});
+		button_arithmetic.setOnClickListener(new View.OnClickListener() {
+			// Creates a selector Dialog Box
+			final CharSequence[] list = { "+", "-", "*", "/" };
+			public void onClick(View v) {
+				AlertDialog.Builder chooserBox = new AlertDialog.Builder(
+						activityContext);
+				chooserBox.setTitle("Select a math operator:");
+				chooserBox.setItems(list,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								scriptInsertAtCursor(list[item]+" ");
+							}
+						});
+				AlertDialog alert = chooserBox.create();
+				alert.show();
+			}
+		});
+		button_forward.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						activityContext);
+				alert.setTitle("Forward");
+				alert.setMessage("Enter the number of units to roll forward:");
+				// Set an EditText view to get user input
+				final EditText input = new EditText(activityContext);
+				input.setInputType(InputType.TYPE_CLASS_NUMBER);
+				alert.setView(input);
+				alert.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								String value = input.getText().toString();
+								scriptInsertAtCursor("fd " + value + " ");
+							}
+						});
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// Nothing Happens if closed
+							}
+						});
+				alert.show();
+			}
+		});
+		button_reverse.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						activityContext);
+				alert.setTitle("Forward");
+				alert.setMessage("Enter the number of units to roll in reverse:");
+				// Set an EditText view to get user input
+				final EditText input = new EditText(activityContext);
+				input.setInputType(InputType.TYPE_CLASS_NUMBER);
+				alert.setView(input);
+				alert.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								String value = input.getText().toString();
+								scriptInsertAtCursor("bk " + value + " ");
+							}
+						});
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// Nothing Happens if closed
+							}
+						});
+				alert.show();
+			}
+		});	
+		button_left.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						activityContext);
+				alert.setTitle("Left");
+				alert.setMessage("Enter the number of units to rotate left:");
+				// Set an EditText view to get user input
+				final EditText input = new EditText(activityContext);
+				input.setInputType(InputType.TYPE_CLASS_NUMBER);
+				alert.setView(input);
+				alert.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								String value = input.getText().toString();
+								scriptInsertAtCursor("lt " + value + " ");
+							}
+						});
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// Nothing Happens if closed
+							}
+						});
+				alert.show();
+			}
+		});
+		button_right.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(
+						activityContext);
+				alert.setTitle("Forward");
+				alert.setMessage("Enter the number of units to rotate right:");
+				// Set an EditText view to get user input
+				final EditText input = new EditText(activityContext);
+				input.setInputType(InputType.TYPE_CLASS_NUMBER);
+				alert.setView(input);
+				alert.setPositiveButton("Ok",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								String value = input.getText().toString();
+								scriptInsertAtCursor("rt " + value + " ");
+							}
+						});
+				alert.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// Nothing Happens if closed
+							}
+						});
+				alert.show();
+			}
+		});
+		button_save.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+			}
+		});
+		button_load.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+
+			}
+		});
+		button_clear.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				scriptClear();
+				Toast toast = Toast.makeText(getApplicationContext(),
+						"Script Cleared", Toast.LENGTH_SHORT);
+				toast.show();
 			}
 		});
-		sample1.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				String string = "if [1==not 0] [fd 100]";
-				scriptClear();
-				scriptAppend(string);
-			}
-		});
-		sample2.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				String string = "v0=3.2 v1=7 v2=v0 v3=45 v4=0 repeat v0[fd v2 rt v3 repeat 4[fd v1]] if 1==-1+2 and 1 or not 1 [fd 100 v4=not v4] if v4 [bk 100 lt v3] if 1>2 [fd 1] if 2>1 [fd 2] if 1<2 [fd 3] if 2<1 [fd 4] if 1<>2 [fd 5] repeat 3 [v4=not v4 if v4<>1 [fd 6]] ;repeat 3 [fd 0 v4=not v4 if v4<>1 [fd 6]] repeat 1.18920711500 * 1.18920711500 * 1.41421356237 fd 3.141592653589793238462";
-				scriptClear();
-				scriptAppend(string);
-			}
-		});
-		sample3.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				String string = "v0=1 v1=2 v2=3 v4=0 repeat [[v1+v2]] [v4=not v4 if [v4 or 0] [fd v0] v0=v0+1]";
-				scriptClear();
-				scriptAppend(string);
-			}
-		});
-		transmit.setOnClickListener(new View.OnClickListener() {
+		button_send.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				transmitScript();
+
 			}
 		});
 
@@ -114,6 +423,8 @@ public class ControlComplexActivity extends BaseActivity {
 		// Prevent keyboard auto-show
 		this.getWindow().setSoftInputMode(
 				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+		// Assigning Context Variable
+		activityContext = this;
 		// Clear TextBox
 		scriptClear();
 		// Create Rover Model
@@ -138,12 +449,28 @@ public class ControlComplexActivity extends BaseActivity {
 	}
 
 	/**
-	 * Sends the script text-box contents to the rover model.
+	 * Inserts the passed-in string to the script at the current cursor
+	 * location.
+	 * 
+	 * @param string
+	 *            Value to Insert
 	 */
-	private void transmitScript(){
-		
-		rover.transmitRogoScript(scriptTextBox.getText().toString());
-		// TODO Get Output, show on screen
+	private void scriptInsertAtCursor(String string) {
+		Editable str = scriptTextBox.getText();
+		String text = string;
+		int len = text.length();
+		str.insert(scriptTextBox.getSelectionStart(), text, 0, len);
 	}
-	
+
+	/**
+	 * Sends the script text-box contents to the rover model. TODO Check for
+	 * valid Bluetooth Connection TODO Get results, show on screen
+	 */
+	private void transmitScript() {
+		rover.transmitRogoScript(scriptTextBox.getText().toString());
+		Toast toast = Toast.makeText(getApplicationContext(),
+				"Script Transmitted", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
 }
