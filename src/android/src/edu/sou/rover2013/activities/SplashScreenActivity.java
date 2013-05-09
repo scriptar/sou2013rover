61c9d1a4aa926aae591423ad6dcd37ad148e65c3
Ryan Dempsey <Ryan5732@gmail.com>
2013-05-09 14:28:33 -0700

    Reworked Splash Screen
    Also changed logo names to be android compatible.

--- src/android/src/edu/sou/rover2013/activities/SplashScreenActivity.java
+++ src/android/src/edu/sou/rover2013/activities/SplashScreenActivity.java
@@ -2,21 +2,61 @@ package edu.sou.rover2013.activities;
 
 import edu.sou.rover2013.BaseActivity;
 import edu.sou.rover2013.R;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import edu.sou.rover2013.activities.ConnectionActivity;
+import edu.sou.rover2013.activities.ControlComplexActivity;
+import edu.sou.rover2013.activities.ControlSimpleActivity;
 
 /**
  * Initial Activity with menu options and instructions on connecting rover
- * 
- * @author Ryan Dempsey
  */
-// TODO Add Instructions
-// TODO Improve theme
+
 public class SplashScreenActivity extends BaseActivity {
+
+	// *******************************
+	// UI Element Variables
+	// *******************************
+	private static Button buttonConnect;
+	private static Button buttonControl;
+	private static Button buttonProgram;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_screen);
+
+		// *******************************
+		// Assigning UI Elements
+		// *******************************
+		buttonConnect = (Button) findViewById(R.id.button_connect);
+		buttonControl = (Button) findViewById(R.id.button_control);
+		buttonProgram = (Button) findViewById(R.id.button_program);
+
+		// *******************************
+		// Button Listeners
+		// *******************************
+		buttonConnect.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), ConnectionActivity.class);
+				startActivity(intent);
+			}
+		});
+		buttonControl.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), ControlSimpleActivity.class);
+				startActivity(intent);
+			}
+		});
+		buttonProgram.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(), ControlComplexActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
